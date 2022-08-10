da1201f8ae9f6a071fa78d3ff4447640edc6e8d8
Geetanjali Sharma <66006481+geetanjali-20@users.noreply.github.com>
2022-08-10 19:16:12 +0530

    Create Subsequence of k sum.cpp

---
+++ DP/Subsequence of k sum.cpp
@@ -0,0 +1,40 @@
+void solve(int i,vector<int> &nums,vector<int> &ds,int k,int s,vector<vector<int>> &ans){
+        if(i == nums.size())
+        {
+            if(k == s)
+                ans.push_back(ds);
+            return;
+        }
+        ds.push_back(nums[i]);
+        s+=nums[i];
+        solve(i+1,nums,ds,k,s,ans);
+        ds.pop_back();
+        s-=nums[i];
+        solve(i+1,nums,ds,k,s,ans);
+    }
+    
+    int numSubseq(vector<int>& nums, int target) {
+        vector<int> ds;
+        vector<vector<int>> ans;
+        solve(0,nums,ds,target,0,ans);
+        
+        for(auto i: ans)
+        {
+            for(auto j: i)
+                cout<<j<<" ";
+            cout<<endl;
+        }    
+        return ans.size();
+    }
+};
+
+
+//Input: nums= [3,5,6,7,2,4]  target=9
+
+//Output:
+// 3 6 
+// 3 2 4 
+// 5 4 
+// 7 2 
+
+//ans=4
